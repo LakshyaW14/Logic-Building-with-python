59ace1f31ae19869f1c3456b34c60bd019df7182
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-03-19 23:29:02 +0530

    add

---
+++ hashing.py
@@ -0,0 +1,67 @@
+
+
+# #Array hashing, when values are in limited range
+#faster than dict ( no hashing overhead)
+
+# mylist=list(map(int,input("Enter numbers :").split(",")))
+
+# queries= list(map(int,input("Enter queries :").split(",")))
+
+# myhash=[0]*101
+
+# for num in mylist:
+#     myhash[num]+=1
+# for q in queries:
+#     print(myhash[q],end=" ") # TC 
+    
+
+# # Basic hashing using dictionary ( frequency count ) 
+# arr = list(map(int,input("Enter numbers :").split(",")))
+# frequency = {}
+# for num in arr :
+#     frequency[num] = frequency.get(num,0) +1  #Tc o(n)
+# print(frequency)   
+
+#Hashing with queries 
+
+# arr=[1,2,3,4,5,6,3,2,]
+# queries =[1,3,2]
+
+# fre={}
+# for num in arr:
+#     fre[num] = fre.get(num,0)+1
+# for q in queries:
+#     que= fre.get(q,0)
+#     print(q, "-" ,que)    # tc (n+q)
+
+
+
+# #Hashing for "check existence only" (Set )
+# arr=[1,2,3,4,5,6,3,2,]
+# s = set(arr)
+
+# print(20 in s) # False Tc (1)
+
+
+# #first non repeating element 
+# arr = [4, 5, 1, 2, 0, 4]
+# fre = {}
+# for num in arr :
+#     fre[num]= fre.get(num,0)+1
+# for num in arr :
+#     if fre[num] == 1:
+#         print("first non_repeating :", num )
+#         break
+
+#
+
+arr = [1,1,2]
+fre = {}
+
+for num in arr:
+    fre[num] = fre.get(num,0)+1
+for num in arr :
+    if int(fre[num]) > 1 :
+        fre[num]= "-"
+        
+print(fre)
